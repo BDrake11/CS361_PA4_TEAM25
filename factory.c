c3d94aa4b979072e41256428ae62e0db9dbdbbfc
Aiden <aidensmith385@gmail.com>
2025-12-01 12:19:43 -0500

    update

--- factory.c
+++ factory.c
@@ -18,62 +18,73 @@
 #include <time.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <sys/time.h>
 
 #include "wrappers.h"
 #include "message.h"
 
-#define MAXSTR     200
+#define MAXSTR      200
 #define IPSTRLEN    50
-#define SEM_NAME "/Team25_mutex"
+#define SEM_NAME    "/Team25_mutex"
 
-// Mutex used when making parts
-sem_t *mutex;
+typedef struct sockaddr SA;
 
-typedef struct sockaddr SA ;
+/* ---------------- Per-sub-factory info that main collects ---------------- */
+typedef struct {
+    int factoryID;      // 1..N
+    int capacity;       // max parts per iteration (10..50)
+    int duration;       // msec per iteration (500..1200)
+    int partsMade;      // total parts this factory made
+    int iterations;     // number of iterations this factory ran
+} FactoryInfo;
 
-int minimum( int a , int b)
+/* ------------- Globals shared with threads (protected as needed) --------- */
+
+// Mutex used when accessing/adjusting remaining work
+sem_t *mutex = NULL;
+
+int   remainsToMake = 0;     // protected by mutex
+int   actuallyMade   = 0;
+int   numActiveFactories = 1;
+int   orderSize = 0;
+
+int sd;                      // server socket descriptor
+struct sockaddr_in srvrSkt;  // address of this server
+struct sockaddr_in clntSkt;  // remote client's socket
+
+/* ------------------------------------------------------------------------ */
+
+int minimum(int a, int b)
 {
-    return ( a <= b ? a : b ) ; 
+    return (a <= b ? a : b);
 }
 
-void subFactory( int factoryID , int myCapacity , int myDuration ) ;
-
-void factLog( char *str )
+void factLog(char *str)
 {
-    printf( "%s" , str );
-    fflush( stdout ) ;
+    printf("%s", str);
+    fflush(stdout);
 }
 
-/*-------------------------------------------------------*/
+/* ----------------------------- Signal handler --------------------------- */
 
-// Global Variable for Future Thread to Shared
-int   remainsToMake , // Must be protected by a Mutex
-      actuallyMade ;  // Actually manufactured items
-
-int   numActiveFactories = 1 , orderSize ;
-
-int   sd ;      // Server socket descriptor
-struct sockaddr_in  
-             srvrSkt,       /* the address of this server   */
-             clntSkt;       /* remote client's socket       */
-
-//------------------------------------------------------------
-//  Handle Ctrl-C or KILL 
-//------------------------------------------------------------
-void goodbye(int sig) 
+void goodbye(int sig)
 {
     /* Mission Accomplished */
-    printf( "\n### I (%d) have been nicely asked to TERMINATE. "
-           "goodbye\n\n" , getpid() );
+    printf("\n### I (%d) have been nicely asked to TERMINATE. goodbye\n\n",
+           getpid());
 
-    // Send a protocol error to the client
+    // Tell the current client (if any) that the protocol ended abruptly
     msgBuf errorBuf;
+    memset(&errorBuf, 0, sizeof(errorBuf));
     errorBuf.purpose = htonl(PROTOCOL_ERR);
-    sendto(sd, &errorBuf, sizeof(errorBuf), 0, (SA *) &clntSkt, sizeof(clntSkt));
-    
+    sendto(sd, &errorBuf, sizeof(errorBuf), 0,
+           (SA *) &clntSkt, sizeof(clntSkt));
+
     // Close and unlink mutex
-    Sem_close(mutex);
-    Sem_unlink(SEM_NAME);
+    if (mutex != NULL) {
+        Sem_close(mutex);
+        Sem_unlink(SEM_NAME);
+    }
 
     // Close socket
     if (close(sd) < 0) {
@@ -84,179 +95,246 @@ void goodbye(int sig)
     exit(0);
 }
 
-/*-------------------------------------------------------*/
-int main( int argc , char *argv[] )
-{
-    char  *myName = "Braden Drake, Aiden Smith" ; 
-    unsigned short port = 50015 ;      /* service port number  */
-    int    N = 1 ;                     /* Num threads serving the client */
+/* ------------------------ Thread routine prototype ---------------------- */
+void *subFactory(void *arg);
 
-    // buffer
+/* ======================================================================== */
+
+int main(int argc, char *argv[])
+{
+    char *myName = "Braden Drake, Aiden Smith";
+    unsigned short port = 50015;   /* service port number  */
+    int N = 1;                     /* Num sub-factory threads per order */
+
     char buf[MAXSTR];
-    // from-address length
     unsigned int alen;
 
-    printf("\nThis is the FACTORY server developed by %s\n\n" , myName ) ;
-    char myUserName[30] ;
-    getlogin_r ( myUserName , 30 ) ;
-    time_t  now;
-    time( &now ) ;
-    fprintf( stdout , "Logged in as user '%s' on %s\n" , myUserName ,  ctime( &now)  ) ;
-    fflush( stdout ) ;
+    printf("\nThis is the FACTORY server developed by %s\n\n", myName);
+    char myUserName[30];
+    getlogin_r(myUserName, 30);
+    time_t now;
+    time(&now);
+    fprintf(stdout, "Logged in as user '%s' on %s\n",
+            myUserName, ctime(&now));
+    fflush(stdout);
 
-    //Ctrl-C and Kill handlers
-    sigactionWrapper(SIGINT, goodbye);
+    // Install Ctrl-C and Kill handlers
+    sigactionWrapper(SIGINT,  goodbye);
     sigactionWrapper(SIGTERM, goodbye);
 
-	switch (argc) 
-	{
-      case 1:
-        break ;     // use default port with a single factory thread
-      
-      case 2:
-        N = atoi( argv[1] ); // get from command line
-        port = 50015;            // use this port by default
-        break;
-
-      case 3:
-        N    = atoi( argv[1] ) ; // get from command line
-        port = atoi( argv[2] ) ; // use port from command line
-        break;
-
-      default:
-        printf( "FACTORY Usage: %s [numThreads] [port]\n" , argv[0] );
-        exit( 1 ) ;
+    /* ------------- Command line: [numThreads] [port] -------------------- */
+    switch (argc) {
+        case 1:
+            break;  // use default port, N=1
+        case 2:
+            N = atoi(argv[1]);
+            port = 50015;
+            break;
+        case 3:
+            N = atoi(argv[1]);
+            port = (unsigned short) atoi(argv[2]);
+            break;
+        default:
+            printf("FACTORY Usage: %s [numThreads] [port]\n", argv[0]);
+            exit(1);
     }
 
-    // Create socket
+    if (N <= 0)
+        N = 1;
+    if (N > MAXFACTORIES)
+        N = MAXFACTORIES;
+
+    /* ------------------------ Set up UDP socket ------------------------- */
     sd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sd < 0)
         err_sys("Could not create socket.");
-    
-    // Prepare the server's socket address structure
-    memset((void *) &srvrSkt, 0, sizeof(srvrSkt));
-    srvrSkt.sin_family = AF_INET;
-    srvrSkt.sin_addr.s_addr = htonl(INADDR_ANY);
-    srvrSkt.sin_port = htons(port);
 
-    // Bind the server to the socket
+    memset((void *) &srvrSkt, 0, sizeof(srvrSkt));
+    srvrSkt.sin_family      = AF_INET;
+    srvrSkt.sin_addr.s_addr = htonl(INADDR_ANY);
+    srvrSkt.sin_port        = htons(port);
+
     if (bind(sd, (SA *) &srvrSkt, sizeof(srvrSkt)) < 0) {
         snprintf(buf, MAXSTR, "Could not bind to port %d", port);
         err_sys(buf);
     }
 
-    // Print IP and port of server
     char ipStr[IPSTRLEN];
     inet_ntop(AF_INET, (void *) &srvrSkt.sin_addr.s_addr, ipStr, IPSTRLEN);
-    printf("\nBound socket %d to IP %s Port %d\n", sd, ipStr, ntohs(srvrSkt.sin_port));
+    printf("\nBound socket %d to IP %s Port %d\n", sd, ipStr,
+           ntohs(srvrSkt.sin_port));
 
-    // Open mutex
+    /* --------------------- Open named semaphore ------------------------ */
     mutex = Sem_open(SEM_NAME, O_CREAT | O_EXCL, S_IRUSR | S_IWUSR, 1);
 
-    int forever = 1;
-    while ( forever )
-    {
-        // Message buf to receive order request
-        alen = sizeof(clntSkt);
-        msgBuf msg1;
-        memset(&msg1, 0, sizeof(msg1));
-        printf( "\nFACTORY server waiting for Order Requests\n\n\n" ) ; 
+    // Seed the random number generator once
+    srand((unsigned int) time(NULL));
 
-        // Receive order request from client
-        if (recvfrom(sd, &msg1, sizeof(msg1), 0, (SA *) &clntSkt, &alen) < 0) {
+    int forever = 1;
+    while (forever) {
+        msgBuf msg1;
+        alen = sizeof(clntSkt);
+        memset(&msg1, 0, sizeof(msg1));
+
+        printf("\nFACTORY server waiting for Order Requests\n\n");
+
+        /* ---------------------- Receive REQUEST_MSG -------------------- */
+        if (recvfrom(sd, &msg1, sizeof(msg1), 0,
+                     (SA *) &clntSkt, &alen) < 0) {
             err_sys("Error during recvfrom()");
         }
 
-        // Print that the factory received an
-        // order request and print the clients IP and port
         printf("FACTORY server received: ");
         printMsg(&msg1);
         puts("");
-        inet_ntop(AF_INET, (void *) &clntSkt.sin_addr.s_addr, ipStr, IPSTRLEN);
-        printf("        From IP %s Port %d\n", ipStr, ntohs(clntSkt.sin_port));
+        inet_ntop(AF_INET, (void *) &clntSkt.sin_addr.s_addr,
+                  ipStr, IPSTRLEN);
+        printf("        From IP %s Port %d\n",
+               ipStr, ntohs(clntSkt.sin_port));
 
-        // Get the order size and assign it to remainsToMake
-        orderSize = ntohl(msg1.orderSize);
-        remainsToMake = orderSize;
+        /* --------------------- Initialize order state ------------------ */
+        orderSize      = (int) ntohl(msg1.orderSize);
+        remainsToMake  = orderSize;
+        numActiveFactories = N;
 
-        // Send an order confirmation to the client with the number of factories
+        /* -------------------- Send ORDR_CONFIRM ------------------------ */
         msg1.purpose = htonl(ORDR_CONFIRM);
-        msg1.numFac = htonl(N);
+        msg1.numFac  = htonl(N);
         sendto(sd, &msg1, sizeof(msg1), 0, (SA *) &clntSkt, alen);
 
-        // Print order confirmation
-        printf("\n\nFACTORY sent this Order Confirmation to the client " );
+        printf("\n\nFACTORY sent this Order Confirmation to the client ");
         printMsg(&msg1);
         puts("");
-        
-        subFactory( 1 , 50 , 350 ) ;  // Single factory, ID=1 , capacity=50, duration=350 ms
+
+        /* ----------------------- Start timing -------------------------- */
+        struct timeval startTime, endTime;
+        gettimeofday(&startTime, NULL);
+
+        /* -------- Create N sub-factory threads with random params ------ */
+        pthread_t   tids[MAXFACTORIES + 1];
+        FactoryInfo finfo[MAXFACTORIES + 1];
+
+        for (int i = 1; i <= N; i++) {
+            finfo[i].factoryID = i;
+            finfo[i].capacity  = 10 + (rand() % 41);   // [10,50]
+            finfo[i].duration  = 500 + (rand() % 701); // [500,1200]
+            finfo[i].partsMade = 0;
+            finfo[i].iterations = 0;
+
+            printf("Created Factory Thread # %d with capacity = %3d parts"
+                   " & duration = %4d mSec\n",
+                   i, finfo[i].capacity, finfo[i].duration);
+
+            Pthread_create(&tids[i], NULL, subFactory, &finfo[i]);
+        }
+
+        /* ------------------- Wait for all sub-factories ---------------- */
+        for (int i = 1; i <= N; i++) {
+            Pthread_join(tids[i], NULL);
+        }
+
+        /* ------------------------ Stop timing -------------------------- */
+        gettimeofday(&endTime, NULL);
+        double elapsed_ms =
+            (endTime.tv_sec  - startTime.tv_sec)  * 1000.0 +
+            (endTime.tv_usec - startTime.tv_usec) / 1000.0;
+
+        /* ---------------------- Print summary report ------------------- */
+        int grandTotal = 0;
+
+        printf("\n****** FACTORY Server ( by Aiden Smith and Braden Drake ) Summary Report ******\n");
+        printf("Sub-Factory   Parts Made   Iterations\n");
+
+        for (int i = 1; i <= N; i++) {
+            grandTotal += finfo[i].partsMade;
+            printf("%6d %13d %11d\n",
+                   finfo[i].factoryID,
+                   finfo[i].partsMade,
+                   finfo[i].iterations);
+        }
+
+        printf("=========================================\n");
+        printf("Grand total parts made = %5d   vs  order size of %5d\n",
+               grandTotal, orderSize);
+        printf("Order-to-Completion time = %.1f milliSeconds\n\n",
+               elapsed_ms);
     }
 
-    // Close and unlink semaphores
+    /* --------------- Clean up if we ever break out of loop ------------- */
     Sem_close(mutex);
     Sem_unlink(SEM_NAME);
 
-    // Close socket
     if (close(sd) < 0) {
         perror("Error closing socket.");
         exit(1);
     }
 
-    return 0 ;
+    return 0;
 }
 
-void subFactory( int factoryID , int myCapacity , int myDuration )
+/* ======================================================================== */
+/*                         Sub-factory thread routine                       */
+/* ======================================================================== */
+
+void *subFactory(void *arg)
 {
-    char    strBuff[ MAXSTR ] ;   // snprint buffer
-    int     partsImade = 0 , myIterations = 0 ;
-    msgBuf  msg;
+    FactoryInfo *info = (FactoryInfo *) arg;
+    char   strBuff[MAXSTR];
+    msgBuf msg;
 
-
-    while ( 1 )
-    {   
-        // Reset message buf
-        memset(&msg, 0, sizeof(msg));
+    while (1) {
         int toMake = 0;
 
-        // See if there are still any parts to manufacture
-        if ( remainsToMake <= 0 )
-            break ;   // Not anymore, exit the loop
-        
-        // Use mutual exclusion when making parts
-        // Increment num of iterations and add total parts made by the factory
+        /* --------- Decide how many parts to make this iteration -------- */
         Sem_wait(mutex);
-        toMake = (remainsToMake >= myCapacity) ? myCapacity : remainsToMake;
+
+        if (remainsToMake <= 0) {
+            // No more work left for anybody
+            Sem_post(mutex);
+            break;
+        }
+
+        toMake = minimum(remainsToMake, info->capacity);
         remainsToMake -= toMake;
-        partsImade += toMake;
-        myIterations++;
+
+        info->partsMade  += toMake;
+        info->iterations += 1;
+
         Sem_post(mutex);
 
-        // Sleep for the duration
-        Usleep((useconds_t) myDuration * 1000);
+        /* ------------- Simulate manufacturing time -------------------- */
+        Usleep((useconds_t) info->duration * 1000);
 
-        // Build production message
-        msg.purpose = htonl(PRODUCTION_MSG);
-        msg.facID = htonl(factoryID);
-        msg.capacity = htonl(myCapacity);
-        msg.partsMade = htonl(toMake);
-        msg.duration = htonl(myDuration);
+        /* ------------------ Send PRODUCTION_MSG ----------------------- */
+        memset(&msg, 0, sizeof(msg));
+        msg.purpose  = htonl(PRODUCTION_MSG);
+        msg.facID    = htonl(info->factoryID);
+        msg.capacity = htonl(info->capacity);
+        msg.partsMade= htonl(toMake);
+        msg.duration = htonl(info->duration);
 
-        // Send a Production Message to client
-        sendto(sd, &msg, sizeof(msg), 0, (SA *) &clntSkt, sizeof(clntSkt));
-        printf("Factory # %2d: Going to make %5d parts in %4d mSec\n", factoryID, toMake, myDuration);
+        sendto(sd, &msg, sizeof(msg), 0,
+               (SA *) &clntSkt, sizeof(clntSkt));
+
+        printf("Factory # %2d: Going to make %5d parts in %4d mSec\n",
+               info->factoryID, toMake, info->duration);
+        fflush(stdout);
     }
 
-    // Completion message buf to send to client
+    /* ------------------ Send COMPLETION_MSG --------------------------- */
     msgBuf done;
     memset(&done, 0, sizeof(done));
-
-    // Build completion message and send it to client
     done.purpose = htonl(COMPLETION_MSG);
-    done.facID = htonl(factoryID);
-    sendto(sd, &done, sizeof(done), 0, (SA *) &clntSkt, sizeof(clntSkt));
+    done.facID   = htonl(info->factoryID);
 
-    snprintf( strBuff , MAXSTR , ">>> Factory # %-3d: Terminating after making total of %-5d parts in %-4d iterations\n" 
-          , factoryID, partsImade, myIterations);
-    factLog( strBuff ) ;
+    sendto(sd, &done, sizeof(done), 0,
+           (SA *) &clntSkt, sizeof(clntSkt));
+
+    snprintf(strBuff, MAXSTR,
+             ">>> Factory # %-3d : Terminating after making total of %-5d"
+             " parts in %-4d iterations\n",
+             info->factoryID, info->partsMade, info->iterations);
+    factLog(strBuff);
+
+    return NULL;
 }
-
--- procurement.c
+++ procurement.c
@@ -18,159 +18,168 @@
 #include <time.h>
 #include <sys/stat.h>
 #include <sys/wait.h>
+#include <sys/time.h>
 
 #include "wrappers.h"
 #include "message.h"
 
-#define MAXFACTORIES    20
-
-typedef struct sockaddr SA ;
+typedef struct sockaddr SA;
 
 /*-------------------------------------------------------*/
-int main( int argc , char *argv[] )
+int main(int argc, char *argv[])
 {
-    int     numFactories ,      // Total Number of Factory Threads
-            activeFactories ,   // How many are still alive and manufacturing parts
-            iters[ MAXFACTORIES+1 ] = {0} ,  // num Iterations completed by each Factory
-            partsMade[ MAXFACTORIES+1 ] = {0} , totalItems = 0;
+    int numFactories,           // total number of factory threads
+        activeFactories,        // how many are still running
+        iters[MAXFACTORIES + 1]     = {0},  // iterations per factory
+        partsMade[MAXFACTORIES + 1] = {0},  // parts per factory
+        totalItems = 0;
 
-    char  *myName = "Braden Drake, Aiden Smith" ; 
-    printf("\nPROCUREMENT: Started. Developed by %s\n\n" , myName );    
+    char *myName = "Braden Drake, Aiden Smith";
+    printf("\nPROCUREMENT: Started. Developed by %s\n\n", myName);
 
-    char myUserName[30] ;
-    getlogin_r ( myUserName , 30 ) ;
-    time_t  now;
-    time( &now ) ;
-    fprintf( stdout , "Logged in as user '%s' on %s\n\n" , myUserName ,  ctime( &now)  ) ;
-    fflush( stdout ) ;
-    
-    if ( argc < 4 )
-    {
-        printf("PROCUREMENT Usage: %s  <order_size> <FactoryServerIP>  <port>\n" , argv[0] );
-        exit( -1 ) ;  
+    char myUserName[30];
+    getlogin_r(myUserName, 30);
+    time_t now;
+    time(&now);
+    fprintf(stdout, "Logged in as user '%s' on %s\n\n",
+            myUserName, ctime(&now));
+    fflush(stdout);
+
+    if (argc < 4) {
+        printf("PROCUREMENT Usage: %s <order_size> <FactoryServerIP> <port>\n",
+               argv[0]);
+        exit(-1);
     }
 
-    unsigned        orderSize  = atoi( argv[1] ) ;
-    char	       *serverIP   = argv[2] ;
-    unsigned short  port       = (unsigned short) atoi( argv[3] ) ;
- 
+    unsigned       orderSize = (unsigned) atoi(argv[1]);
+    char          *serverIP  = argv[2];
+    unsigned short port      = (unsigned short) atoi(argv[3]);
+
     printf("Attempting Factory server at '%s' : %d\n", serverIP, port);
 
-    /* Set up local and remote sockets */
+    /* ------------------------- Set up socket --------------------------- */
     int sd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sd < 0)
         err_sys("Could not create socket.");
 
-    // Prepare the server's socket address structure
     struct sockaddr_in srvrSkt;
     memset((void *) &srvrSkt, 0, sizeof(srvrSkt));
     srvrSkt.sin_family = AF_INET;
-    srvrSkt.sin_port = htons(port);
+    srvrSkt.sin_port   = htons(port);
 
-    // Give the srvrSkt the provided IP address
-    if (inet_pton(AF_INET, serverIP, (void *) &srvrSkt.sin_addr.s_addr) != 1)
+    if (inet_pton(AF_INET, serverIP,
+                  (void *) &srvrSkt.sin_addr.s_addr) != 1)
         err_sys("Invalid server IP address");
 
-    // Build and send the initial request to the Factory Server
-    msgBuf  msg1;
+    /* ---------------------- Send REQUEST_MSG --------------------------- */
+    msgBuf msg1;
     memset(&msg1, 0, sizeof(msg1));
-    msg1.purpose = htonl(REQUEST_MSG);
+    msg1.purpose   = htonl(REQUEST_MSG);
     msg1.orderSize = htonl(orderSize);
+
     sendto(sd, &msg1, sizeof(msg1), 0, (SA *) &srvrSkt, sizeof(srvrSkt));
 
-    // Print message
-    printf("\nPROCUREMENT Sent this message to the FACTORY server: "  );
-    printMsg( & msg1 );
+    printf("\nPROCUREMENT Sent this message to the FACTORY server: ");
+    printMsg(&msg1);
     puts("");
 
-    /* Now, wait for order confirmation from the Factory server */
-    msgBuf  msg2;
+    /* ---------------- Wait for ORDR_CONFIRM from server ---------------- */
+    msgBuf msg2;
     memset(&msg2, 0, sizeof(msg2));
     unsigned int alen = sizeof(srvrSkt);
 
-    printf ("\nPROCUREMENT is now waiting for order confirmation ...\n" );
+    printf("\nPROCUREMENT is now waiting for order confirmation ...\n");
 
-    // Receive order confirmation
-    if (recvfrom(sd, &msg2, sizeof(msg2), 0, (SA *) &srvrSkt, &alen) < 0)
+    if (recvfrom(sd, &msg2, sizeof(msg2), 0,
+                 (SA *) &srvrSkt, &alen) < 0)
         err_sys("Error during recvfrom()");
-    
-    printf("PROCUREMENT received this from the FACTORY server: "  );
-    printMsg( & msg2 ); 
+
+    printf("PROCUREMENT received this from the FACTORY server: ");
+    printMsg(&msg2);
     puts("\n");
 
-    // Get the number of factories and assign it to activeFactories
-    numFactories = ntohl(msg2.numFac);
+    /* ---------------- Start timing at order confirmation --------------- */
+    struct timeval startTime, endTime;
+    gettimeofday(&startTime, NULL);
+
+    numFactories    = (int) ntohl(msg2.numFac);
+    if (numFactories > MAXFACTORIES)
+        numFactories = MAXFACTORIES;
     activeFactories = numFactories;
 
-    // Incoming production message buf
+    /* ------- Collect PRODUCTION & COMPLETION messages from factories --- */
     msgBuf incomingMessage;
     unsigned int srvrLen = sizeof(srvrSkt);
-    // Monitor all Active Factory Lines & Collect Production Reports
-    while ( activeFactories > 0 ) // wait for messages from sub-factories
-    {
-        // Clear the buffer
+
+    while (activeFactories > 0) {
         memset(&incomingMessage, 0, sizeof(incomingMessage));
 
-        // Receive a message
-        if (recvfrom(sd, &incomingMessage, sizeof(incomingMessage), 0, (SA *) &srvrSkt, &srvrLen) < 0)
+        if (recvfrom(sd, &incomingMessage, sizeof(incomingMessage), 0,
+                     (SA *) &srvrSkt, &srvrLen) < 0)
             err_sys("Error during recvfrom()");
 
-        // Get the purpose and factory ID
         int purpose = ntohl(incomingMessage.purpose);
-        int facID = ntohl(incomingMessage.facID);
+        int facID   = (int) ntohl(incomingMessage.facID);
 
         if (purpose == PRODUCTION_MSG) {
-            // If it's a production message, print the factory ID,
-            // number of parts made, and the duration
-            int parts = ntohl(incomingMessage.partsMade);
-            int duration = ntohl(incomingMessage.duration);
-            printf("PROCUREMENT: Factory #%-2d  produced %-5d parts in %-4d  milliSecs\n", facID, parts, duration);
+            int parts    = (int) ntohl(incomingMessage.partsMade);
+            int duration = (int) ntohl(incomingMessage.duration);
 
-            // Also increase iterations and the parts made from the factory
+            printf("PROCUREMENT: Factory #%-2d  produced %-5d parts"
+                   " in %-4d milliSecs\n",
+                   facID, parts, duration);
+
             iters[facID]++;
             partsMade[facID] += parts;
-        } else if (purpose == COMPLETION_MSG) {
-            // If it's a completion message, simply print that
-            // the factory completed and decrement activeFactories
-            printf("PROCUREMENT: Factory #%-2d        COMPLETED its task\n\n", facID);
+        }
+        else if (purpose == COMPLETION_MSG) {
+            printf("PROCUREMENT: Factory #%-2d        COMPLETED its task\n\n",
+                   facID);
             activeFactories--;
-        } else if (purpose == PROTOCOL_ERR) {
-            // If it's a protocol error message, that means the server stopped (Caught a SIGINT or SIGTERM).
-            // Print a protocol error message, close the socket, and exit
+        }
+        else if (purpose == PROTOCOL_ERR) {
             printf("PROCUREMENT: Received invalid msg ");
             printMsg(&incomingMessage);
             puts("\n");
-            
+
             if (close(sd) < 0)
                 perror("Error closing socket.");
-            
             exit(1);
-        }       
-    } 
-
-    // Print the summary report
-    totalItems  = 0 ;
-    printf("\n****** PROCUREMENT Summary Report ******\n");
-
-    // Get the total number of items made and prints how many
-    // parts each factory produced and in how many iterations
-    for (int i = 1; i <= numFactories; i++) {
-        totalItems += partsMade[i];
-
-        printf("Factory # %2d made a total of %5d parts in %5d iterations\n", i, partsMade[i], iters[i]);
+        }
+        else {
+            // Ignore any unexpected message types
+        }
     }
 
-    printf("==============================\n") ;
+    /* ---------------------- Stop timing -------------------------------- */
+    gettimeofday(&endTime, NULL);
+    double elapsed_ms =
+        (endTime.tv_sec  - startTime.tv_sec)  * 1000.0 +
+        (endTime.tv_usec - startTime.tv_usec) / 1000.0;
 
-    printf("Grand total parts made = %5d   vs  order size of %5d\n", totalItems, orderSize);
+    /* ---------------------- Print summary report ----------------------- */
+    totalItems = 0;
+    printf("\n****** PROCUREMENT (by Aiden Smith and Braden Drake ) Summary Report ******\n");
+    printf("Sub-Factory   Parts Made   Iterations\n");
 
-    printf( "\n>>> PROCURMENT Terminated\n");
+    for (int i = 1; i <= numFactories; i++) {
+        totalItems += partsMade[i];
+        printf("%6d %13d %11d\n",
+               i, partsMade[i], iters[i]);
+    }
 
-    // Close socket
+    printf("=========================================\n");
+    printf("Grand total parts made = %5d   vs  order size of %5d\n",
+           totalItems, orderSize);
+    printf("Order-to-Completion time = %.1f milliSeconds\n",
+           elapsed_ms);
+
+    printf("\n>>> PROCURMENT Terminated\n");
+
     if (close(sd) < 0) {
         perror("Error closing socket.");
         exit(1);
     }
 
-    return 0 ;
+    return 0;
 }
